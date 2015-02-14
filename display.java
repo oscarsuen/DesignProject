16188c984e20b91e3cf37f356839a27faea10a91
StuartRucker <Stuart.a.rucker@gmail.com>
2015-02-14 16:43:51 -0500

    Easier to add pixels
    With the mouse change listener, you can drag your mouse across the screen and add pixels, as well as just clicking them.

--- display.java
+++ display.java
@@ -2,16 +2,20 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 
 import javax.swing.JPanel;
 
-public class display extends JPanel implements MouseListener {
+public class display extends JPanel implements MouseListener, MouseMotionListener {
 	boolean[][] points = new boolean[120][120];
-
+	int lastXMouse = -1;
+	int lastYMouse = -1;
+	
 	public display() {
 		addMouseListener(this);
+		addMouseMotionListener(this);
 		setVisible(true);
-
+		setBackground(Color.gray);
 		// for testing purposes
 		for (int x = 0; x < 120; x++) {
 			for (int y = 0; y < 120; y++) {
@@ -25,22 +29,24 @@ public class display extends JPanel implements MouseListener {
 
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		g.setColor(Color.GRAY);
-		g.fillRect(0, 0, 600, 600);
-		g.setColor(Color.pink);
+		
 		for (int x = 0; x < 120; x++) {
 			for (int y = 0; y < 120; y++) {
-				if (points[x][y])
-					g.fillRect(5 * x, 5 * y, 5, 5);
+					if(points[x][y]){
+						g.setColor(Color.pink);
+						g.fillRect(5 * x, 5 * y, 5, 5);
+					}
+				}
 			}
 		}
-	}
+
 
 	public void updateArray(boolean[][] s) {
 		points = s;
 		paintComponent(getGraphics());
 	}
 
+	
 	public void mouseReleased(MouseEvent e) {
 		points[e.getX() / 5][e.getY() / 5] = !points[e.getX() / 5][e.getY() / 5];
 		repaint();
@@ -61,5 +67,20 @@ public class display extends JPanel implements MouseListener {
 	}
 
 	public void mouseExited(MouseEvent e) {
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		if(e.getX()/5 != lastXMouse/5 || e.getX()/5 != lastYMouse/5){
+			points[e.getX() / 5][e.getY() / 5] = !points[e.getX() / 5][e.getY() / 5];
+			repaint();
+			lastXMouse = e.getX();
+			lastYMouse = e.getY();
+		}
+	}
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 }
